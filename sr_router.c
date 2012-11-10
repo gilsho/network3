9cfdd3408e6737364a88fd6f278521f725255ba8
Gil Shotan <gilsho@myth27.Stanford.EDU>
2012-11-10 06:52:45 -0800

    fixed bug associated with last commit

--- sr_router.c
+++ sr_router.c
@@ -380,23 +380,23 @@ void wrap_ip_packet(struct sr_instance *sr,uint8_t *payload, unsigned int pyldle
 
 void send_ICMP_ttl_exceeded(struct sr_instance *sr, sr_ip_hdr_t *recv_iphdr,sr_if_t *iface)
 {
-  	sr_icmp_hdr_t *icmphdr = (sr_icmp_hdr_t *) malloc(ICMP_PACKET_SIZE);
-  	memset(icmphdr,0,ICMP_PACKET_SIZE);
+  	sr_icmp_t3_hdr_t *icmp3hdr = (sr_icmp_t3_hdr_t *) malloc(ICMP_PACKET_SIZE);
+  	memset(icmp3hdr,0,ICMP_PACKET_SIZE);
 
   	memcpy(&icmp3hdr->data,recv_iphdr,ICMP_DATA_SIZE);
 
-	icmphdr->icmp_type = icmp_type_ttl_expired;
-	icmphdr->icmp_code = icmp_code_ttl_expired_in_transit;
+	icmp3hdr->icmp_type = icmp_type_ttl_expired;
+	icmp3hdr->icmp_code = icmp_code_ttl_expired_in_transit;
 	
-	icmphdr->icmp_sum = 0;
-	icmphdr->icmp_sum = cksum(icmphdr,ICMP_PACKET_SIZE);
+	icmp3hdr->icmp_sum = 0;
+	icmp3hdr->icmp_sum = cksum(icmp3hdr,ICMP_PACKET_SIZE);
 
 	uint32_t sip = iface->ip;
 	uint32_t dip = recv_iphdr->ip_src;
 
-	wrap_ip_packet(sr,(uint8_t *)icmphdr,ICMP_PACKET_SIZE,sip,dip,ip_protocol_icmp,iface);
+	wrap_ip_packet(sr,(uint8_t *)icmp3hdr,ICMP_PACKET_SIZE,sip,dip,ip_protocol_icmp,iface);
 
-	free(icmphdr);
+	free(icmp3hdr);
 }
 
 void send_ICMP_host_unreachable(struct sr_instance *sr,sr_ip_hdr_t *recv_iphdr, sr_if_t *iface)
